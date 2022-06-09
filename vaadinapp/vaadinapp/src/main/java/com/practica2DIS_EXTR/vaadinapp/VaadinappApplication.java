e86ff925c084f01c47e7f2925733db50d13187de
Veronica <veronicaciancia@gmail.com>
2022-06-09 17:41:42 +0200

    primer commit

---
+++ vaadinapp/vaadinapp/src/main/java/com/practica2DIS_EXTR/vaadinapp/VaadinappApplication.java
@@ -0,0 +1,13 @@
+package com.practica2DIS_EXTR.vaadinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinappApplication.class, args);
+	}
+
+}
