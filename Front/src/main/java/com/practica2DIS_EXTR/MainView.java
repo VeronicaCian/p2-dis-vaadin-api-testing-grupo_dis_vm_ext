5acd8807fc64af4600077d9d2c11127586e6c3d9
migueels <salasrmcf1997@gmail.com>
2022-06-15 17:41:50 +0200

    commit desde 0 el front

--- Front/src/main/java/com/practica2DIS_EXTR/MainView.java
+++ Front/src/main/java/com/practica2DIS_EXTR/MainView.java
@@ -1,17 +1,16 @@
 package com.practica2DIS_EXTR;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import com.practica2DIS_EXTR.Clases.Equipos;
 import com.practica2DIS_EXTR.Clases.Prestamos;
 import com.practica2DIS_EXTR.Clases.Usuarios;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
@@ -20,9 +19,6 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
 
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -35,120 +31,165 @@ import java.util.concurrent.atomic.AtomicInteger;
 @PWA(name = "My Application", shortName = "My Application")
 public class MainView extends VerticalLayout {
 
-
-    private static final String URL = "http://localhost:8080/api/%s";
-
-
-
-
+    //declaramos las variables final para el proyecto
     final VerticalLayout layout;
-    final Tab usersTAB;
-    final Tab prestTAB;
-    final Tab equipoTAB;
+    final Tab pestañaUser;
+    final Tab pestañaPrestamo;
+    final Tab pestañaEquipo;
     final Map<Tab, Component> tabsToPages = new HashMap<>();
     final Tabs tabs;
 
-
-    public MainView() {
-
+    public MainView(){
 
 
-        //primeramente nos creamos un grid con los usuarios
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        ArrayList<Usuarios> users;
-        Type ListaUsersType = new TypeToken<ArrayList<Usuarios>>(){}.getType();
-        //users = gson.fromJson(usersST, ListaUsersType);
+        //objetos inciales
+        VerticalLayout totallayout = new VerticalLayout();
 
 
-        //Layout de usuarios
-        VerticalLayout usersVLayout = new VerticalLayout();
-        HorizontalLayout usersHLayout = new HorizontalLayout();
+        //INICIO LAYOUT USUARIOS
 
-        //grid usuarios
+        VerticalLayout VerticalUsersLayout = new VerticalLayout();
+        HorizontalLayout HorizontalUsersLayout = new HorizontalLayout();
 
-        Grid<Usuarios> gridUsers = new Grid<>(Usuarios.class);
+        //hacemos referencia a la pestaña de usuarios --> tab
+        pestañaUser = new Tab(VaadinIcon.USER.create(),new Span("Usuarios"));
+        Div divUsers = new Div();
+
+        //nos creamos el boton de añadir un nuevo usuario
+        Button btnNewUser = new Button("Nuevo User");
+
+
+        //Inicio grid Usuarios
+
+        Grid<Usuarios> UsersGrid = new Grid<>(Usuarios.class);
+        UsersGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
         //gridUsers.setItems(users);
-        gridUsers.removeColumnByKey("id");
-        gridUsers.setColumns("nombre", "departamento", "telefono", "email", "ubicacion");
+        UsersGrid.removeColumnByKey("id");
+        UsersGrid.setColumns("nombre", "departamento", "telefono", "email", "ubicacion");
+        UsersGrid.addColumn(Usuarios::getNombre);
+        UsersGrid.addColumn(Usuarios::getDepartamento);
+        UsersGrid.addColumn(Usuarios::getTelefono);
+        UsersGrid.addColumn(Usuarios::getEmail);
+        UsersGrid.addColumn(Usuarios::getUbicacion);
 
-        //tabshet y div de usuarios
-        usersTAB = new Tab(VaadinIcon.USER.create(),new Span("Usuarios"));
-        Div usersDIV = new Div();
+        //al pulsar el boton de nuevo usuario
+        btnNewUser.addClickListener( e ->{
+            Dialog dialog = new Dialog();
+            VerticalLayout dialogLayout = createDialogUserLayout();
+            dialog.add(dialogLayout);
 
-        //Definicion de los textfield
-        AtomicInteger id_user = new AtomicInteger();
-        TextField nombre = new TextField("Nombre");
-        TextField departamento = new TextField("Departamento");
-        TextField telefono = new TextField("Telefono");
-        TextField email = new TextField("Email");
-        TextField ubicacion = new TextField("Ubicación");
-
-        //botones del grid usuarios
-        Button anadir = new Button("Añadir");
-        Button acep_2 = new Button("Aceptar_2");
-        Button nuevo = new Button("Nuevo User");
-        Button edit = new Button("Editar User");
-        Button borrar = new Button("Borrar User");
-
-        usersHLayout.add(nombre, departamento, telefono, email, ubicacion);
-        usersVLayout.add(usersHLayout, nuevo);
-        usersDIV.add(gridUsers, nuevo);
+            Button cancelButton = new Button("Cancel", e1 -> dialog.close());
+            dialog.add(cancelButton);
 
 
+            dialog.open();
 
-        //PRESTAMOS
-        prestTAB = new Tab(VaadinIcon.DOLLAR.create(),new Span("Prestamos"));
-        Div prestDIV = new Div();
-        prestDIV.setVisible(false);
+            // Center the button within the example
+            getStyle().set("position", "fixed").set("top", "0").set("right", "0")
+                    .set("bottom", "0").set("left", "0").set("display", "flex")
+                    .set("align-items", "center").set("justify-content", "center");
+        });
 
-        //Definicion del grid de users
-        Grid<Prestamos> gridPrest = new Grid<>(Prestamos.class);
-        //gridPrest.setItems(prestamos);
-        gridPrest.removeColumnByKey("id");
-        //gridPrest.setColumns("Usuario_Id","Fecha_Inicio_Prestamo", "Fecha_Fin_Prestamo", "Fecha_Real_Dev", "Comentarios");
+        //fin grid Usuarios
 
-        //Definicion de los layaout
-        VerticalLayout prestVLayout = new VerticalLayout();
-        HorizontalLayout prestHLayout = new HorizontalLayout();
-
-        //Definicion de los textfield
-        TextField iduser = new TextField("id_user");
-        TextField idequipo = new TextField("id_equipo");
-        TextField fechaIni = new TextField("fecha_inicio");
-        TextField fechaFin = new TextField("fecha_fin");
-        TextField fechaReal = new TextField("fecha_real");
-        TextField comen = new TextField("comentario");
-
-        //Definicion de los buttons
-        Button anadir_1 = new Button("Añadir");
-        Button acep_2_2 = new Button("Aceptar_2");
-        Button nuevo_2 = new Button("Nuevo Prestamo");
-        Button edit_2 = new Button("Editar Prestamo");
+        HorizontalUsersLayout.add(btnNewUser);
+        VerticalUsersLayout.add(UsersGrid, HorizontalUsersLayout);
+        divUsers.add(VerticalUsersLayout);
+        divUsers.getStyle().set("flex-wrap", "wrap");
 
 
-        //Definicion del diseño de los layout y divs
-        prestVLayout.add(prestHLayout, nuevo_2);
-        prestHLayout.add(iduser, idequipo, fechaIni, fechaFin, fechaReal, comen);
-        prestDIV.add(gridPrest, nuevo_2);
+        //FIN LAYOUT USUARIOS
 
 
 
-        //EQUIPOS
-        equipoTAB = new Tab(VaadinIcon.DESKTOP.create(),new Span("Equipos"));
-        Div equipoDIV = new Div();
-        equipoDIV.setVisible(false);
 
 
-        //tabshett map
 
-        tabsToPages.put(usersTAB, usersDIV);
-        tabsToPages.put(prestTAB, prestDIV);
-        tabsToPages.put(equipoTAB,equipoDIV);
 
-        tabs = new Tabs(usersTAB, prestTAB,equipoTAB);
+        //INICIO LAYOUT EQUIPOS
+
+        VerticalLayout VerticalEquipoLayout = new VerticalLayout();
+        HorizontalLayout HorizontalEquipoLayout = new HorizontalLayout();
+
+        //hacemos referencia a la pestaña de usuarios --> tab
+        pestañaEquipo = new Tab(VaadinIcon.DESKTOP.create(),new Span("Equipos"));
+        Div divEquipos = new Div();
+
+        //nos creamos el boton de añadir un nuevo prestamo
+        Button btnNewEquipo = new Button("Nuevo Prestamo");
+
+        //Inicio Grid Equipos
+
+        Grid<Equipos> EquipoGrid = new Grid<>(Equipos.class);
+        UsersGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        //EquipoGrid.removeColumnByKey("id_equipo");
+        EquipoGrid.setColumns("tipo","marca","uso");
+        EquipoGrid.addColumn(Equipos::getTipo);
+        EquipoGrid.addColumn(Equipos::getMarca);
+        EquipoGrid.addColumn(Equipos::getUso);
+
+
+        //Fin Grid equipos
+
+
+        VerticalEquipoLayout.add(EquipoGrid,HorizontalEquipoLayout);
+        divEquipos.add(VerticalEquipoLayout);
+        divEquipos.getStyle().set("flex-wrap", "wrap");
+
+        //FIN LAYOUT EQUIPOS
+
+
+
+
+
+
+
+
+
+        //INICIO LAYOUT PRESTAMOS
+
+        VerticalLayout VerticalPrestamoLayout = new VerticalLayout();
+        HorizontalLayout HorizontalPrestamoLayout = new HorizontalLayout();
+
+        //hacemos referencia a la pestaña de usuarios --> tab
+        pestañaPrestamo = new Tab(VaadinIcon.DOLLAR.create(),new Span("Prestamos"));
+        Div divPrestamos = new Div();
+
+        //nos creamos el boton de añadir un nuevo prestamo
+        Button btnNewPrestamo = new Button("Nuevo Prestamo");
+
+        //Incicio de grid Prestamos
+
+        Grid<Prestamos> PrestamoGrid = new Grid<>(Prestamos.class);
+        PrestamoGrid.removeColumnByKey("id");
+        PrestamoGrid.setColumns("usuario_Id","fecha_Inicio_Prestamo","fecha_Fin_Prestamo","comentarios");
+        PrestamoGrid.addColumn(Prestamos::getUsuario_Id);
+        PrestamoGrid.addColumn(Prestamos::getFecha_Inicio_Prestamo);
+        PrestamoGrid.addColumn(Prestamos::getFecha_Fin_Prestamo);
+        PrestamoGrid.addColumn(Prestamos::getComentarios);
+
+        //Fin del grid Prestamos
+
+        VerticalPrestamoLayout.add(PrestamoGrid,btnNewPrestamo);
+        divPrestamos.add(VerticalPrestamoLayout);
+        VerticalUsersLayout.getStyle().set("flex-wrap", "wrap");
+
+
+        //FIN LAYOUT PRESTAMOS
+
+
+
+
+        //INCIO TABSHEET
+
+        tabsToPages.put(pestañaUser,divUsers);
+        tabsToPages.put(pestañaEquipo,divEquipos);
+        tabsToPages.put(pestañaPrestamo,divPrestamos);
+
+        tabs = new Tabs(pestañaUser, pestañaEquipo,pestañaPrestamo);
         tabs.setSizeFull();
-        Div pages = new Div(usersDIV, prestDIV,equipoDIV);
-        pages.setSizeFull();
+        Div paginas = new Div(divUsers, divEquipos,divPrestamos);
+        paginas.setSizeFull();
 
 
         tabs.addSelectedChangeListener(event ->{
@@ -162,26 +203,51 @@ public class MainView extends VerticalLayout {
         layout.setSpacing(false);
         setLayout(tabs.getSelectedTab());
 
-        add(tabs, pages,layout);
+        add(tabs, paginas,layout);
+
+        //FIN TABSHEET
 
     }
+
+
+    //Inicio de los metodos
 
     private void setLayout(Tab tab) {
         layout.removeAll();
 
-        if (tab.equals(usersTAB)) {
+        if (tab.equals(pestañaUser)) {
             tabsToPages.values().forEach(page -> page.setVisible(false));
             Component paginaSeleccionada = tabsToPages.get(tabs.getSelectedTab());
             paginaSeleccionada.setVisible(true);
-        } else if (tab.equals(prestTAB)) {
+        } else if (tab.equals(pestañaEquipo)) {
             tabsToPages.values().forEach(page -> page.setVisible(false));
             Component paginaSeleccionada = tabsToPages.get(tabs.getSelectedTab());
             paginaSeleccionada.setVisible(true);
-        } else if (tab.equals(equipoTAB)){
+        } else if (tab.equals(pestañaPrestamo)){
             tabsToPages.values().forEach(page -> page.setVisible(false));
             Component paginaSeleccionada = tabsToPages.get(tabs.getSelectedTab());
             paginaSeleccionada.setVisible(true);
         }
+    }
+
+    private static VerticalLayout createDialogUserLayout() {
+
+        //Definicion de los textfield
+        AtomicInteger id_user = new AtomicInteger();
+        TextField nombre = new TextField("Nombre");
+        TextField departamento = new TextField("Departamento");
+        TextField telefono = new TextField("Telefono");
+        TextField email = new TextField("Email");
+        TextField ubicacion = new TextField("Ubicación");
+
+        VerticalLayout dialogLayout = new VerticalLayout(
+                nombre,departamento,telefono, email, ubicacion);
+        dialogLayout.setPadding(false);
+        dialogLayout.setSpacing(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        dialogLayout.getStyle().set("width", "18rem").set("max-width", "100%");
+
+        return dialogLayout;
     }
 
 
